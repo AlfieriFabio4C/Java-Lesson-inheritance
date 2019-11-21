c11f975e4de1132fd84d4f939dcfcb3469cd3f2d
AlfieriFabio4C <56158271+AlfieriFabio4C@users.noreply.github.com>
2019-11-21 16:58:58 +0100

    Update EsercizioContoBancario.java

--- EsercizioContoBancario.java
+++ EsercizioContoBancario.java
@@ -5,6 +5,8 @@
  */
 package eserciziocontobancario;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author alfieri.fabio
@@ -15,7 +17,7 @@ public class EsercizioContoBancario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ContoEsteso a = new ContoEsteso(20, 30,"365");
+        ContoEsteso a = new ContoEsteso(20, 30, "365");
         System.out.println(a.getBilancio());
         System.out.println(a.getNumeroConto());
         System.out.println(a.getFido());
@@ -23,5 +25,20 @@ public class EsercizioContoBancario {
         System.out.println(a.getBilancio());
         a.prelievo();
         System.out.println(a.getBilancio());
+        ContoEsteso b = new ContoEsteso(48, "555");
+        System.out.println(a.getBilancio());
+        System.out.println(a.getNumeroConto());
+        System.out.println(a.getFido());
+        a.deposito();
+        System.out.println(a.getBilancio());
+        a.prelievo();
+        ContoEsteso c = new ContoEsteso("666");
+        System.out.println(a.getBilancio());
+        System.out.println(a.getNumeroConto());
+        System.out.println(a.getFido());
+        a.deposito();
+        System.out.println(a.getBilancio());
+        a.prelievo();
+        
     }
 }
